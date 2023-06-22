30ecc38a539202d5e784262af532f7e4baf7b498
Jemark <jemark.severino@gmail.com>
2023-06-22 09:02:49 +0800

    Uploding files
    Initial commit

---
+++ src/WebUI/ClientApp/src/app/todo/todo.components.color.enums.ts
@@ -0,0 +1,11 @@
+export enum Colours
+{
+  White = "#FFFFFF",
+  Red = "#FF5733",
+  Orange = "#FFC300",
+  Yellow = "#FFFF66",
+  Green = "#CCFF99",
+  Blue = "#6666FF",
+  Purple = "#9966CC",
+  Grey = "#999999"
+}
